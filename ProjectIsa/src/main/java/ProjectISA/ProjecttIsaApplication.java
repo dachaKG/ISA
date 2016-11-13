b224c872740f13268c59b51af675c56bf3e3757e
Vladimir <vladimir94@gmail.com>
2016-11-13 16:53:26 +0100

    First commit

---
+++ ProjectIsa/src/main/java/ProjectISA/ProjecttIsaApplication.java
@@ -0,0 +1,13 @@
+package ProjectISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttIsaApplication.class, args);
+		System.out.println("pocetak");
+	}
+}
---
+++ ProjectIsa/src/test/java/ProjectISA/ProjecttIsaApplicationTests.java
@@ -0,0 +1,16 @@
+package ProjectISA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjecttIsaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
