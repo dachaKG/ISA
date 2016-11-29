d5d98e1e64fb6d6f7ba9d047df0fae2d189b2331
dachaKG <choda.94@gmail.com>
2016-11-30 00:04:08 +0100

    Testiram2.

---
+++ ProjectIsa/src/main/java/ProjectISA/User.java
@@ -0,0 +1,23 @@
+package ProjectISA;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	
+	@Column
+	String name;
+	
+}
