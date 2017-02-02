d665a088d5f402e7b68d0b0e0a59d50e1ec2b9d9
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2017-02-02 17:12:08 +0100

    mmm

--- ProjectIsa/src/main/java/app/restaurant/Table.java
+++ ProjectIsa/src/main/java/app/restaurant/Table.java
@@ -14,11 +14,18 @@ import lombok.Data;
 @Data
 @Entity
 public class Table {
+	
+	public static final int NOT_EXISTS = 0;
+	public static final int EXISTS = 1;
+	public static final int RESERVED = 2;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "TABLE_ID")
 	private Long id;
+	
+	@Column
+	private String name;
 	
 	@ManyToOne
 	@JoinColumn(name = "SEGMENT_ID")
@@ -27,5 +34,22 @@ public class Table {
 	@ManyToOne
 	@JoinColumn(name = "ORDER_ID")
 	private Orderr order;
+	
+	@Column
+	private int xPos;
+	
+	@Column 
+	private int yPos;
+	
+	@Column
+	private int status;
+	
+	public Table(String name, Segment segment, int xPos, int yPos, int status){
+		this.name = name;
+		this.segment = segment;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.status = status;
+	}
 	
 }
