ce4e8628fa8158691911b8fc3a5c1fb6edbddbd6
Vladimir <vladimir94@gmail.com>
2017-03-01 13:03:24 +0100

    Izbaceno eager.

--- ProjectIsa/src/main/java/app/restaurant/Restaurant.java
+++ ProjectIsa/src/main/java/app/restaurant/Restaurant.java
@@ -87,7 +87,7 @@ public class Restaurant {
 	
 	
 	//lista porudzbina restorana za ponudjace
-	@OneToMany(fetch = FetchType.EAGER)	// Sa ovim nece da pokrene aplikaciju
+	@OneToMany
 	//@OneToMany
 	@JoinTable(name = "RESTAURANT_RESTAURANT_ORDER", joinColumns = @JoinColumn(name = "RESTAURANT_ID"), inverseJoinColumns = @JoinColumn(name = "RESTAURANT_ORDER_ID"))
 	private List<RestaurantOrderr> restaurantOrders;
